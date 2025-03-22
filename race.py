3253cb0e692ee625e601cb6153012a72b184a0e6
Laucian1 <resmithjr88@gmail.com>
2025-03-22 13:27:43 -0600

    Finished CoreRace classes

--- race.py
+++ race.py
@@ -112,7 +112,7 @@ class Dwarf(CharacterRace):
             "Wisdom" : 2,
             "Charisma" : -2
         }
-        self.subtype = "(dwarf)"
+        self.subtype = ["dwarf"]
         self.speed = 20
         self.languages.append("Dwarven")
         self.senses = {"Darkvision": "Dwarves can see perfectly in the dark up to 60 feet."}
@@ -138,7 +138,7 @@ class Elf(CharacterRace):
             "Constitution" : -2,
             "Intelligence" : 2
         }
-        self.subtype = "(elf)"
+        self.subtype = ["elf"]
         self.languages.append("Elven")
         self.senses = {"Low-Light Vision": "Elves can see twice as far as humans in conditions of dim light."}
         self.racial_abilities = [
@@ -151,6 +151,108 @@ class Elf(CharacterRace):
         ]
         self.race_points = 10
 
+class Gnome(CharacterRace):
+    def __init__(self):
+        super().__init__()
+        self.name = CoreRace.GNOME.value 
+        self.ability_score_modifiers = {
+            "Strength": -2,
+            "Constitution": 2,
+            "Charisma": 2,
+        }
+        self.size = "Small"
+        self.subtype = ["gnome"]
+        self.speed = 20
+        self.languages.extend(["Gnome", "Sylvan"])
+        self.senses = {"Low Light Vision": "Gnomes can see twice as far as humans in conditions of dim light."}
+        self.racial_abilities = [
+            {"Defensive Training": "Gnomes gain a +4 dodge bonus to AC against monsters of the giant subtype."},
+            {"Illusion Resistance": "Gnomes gain a +2 racial saving throw bonus against illusion spells and effects."},
+            {"Gnome Magic": "Gnomes add +1 to the DC of any saving throws against illusion spells that they cast. Gnomes with Charisma scores of 11 or higher also gain the following spell-like abilities: 1/day—dancing lights, ghost sound, prestidigitation, and speak with animals. The caster level for these effects is equal to the gnome’s level. The DC for these spells is equal to 10 + the spell’s level + the gnome’s Charisma modifier."},
+            {"Hatred": "Gnomes receive a +1 bonus on attack rolls against humanoid creatures of the reptilian and goblinoid subtypes because of their special training against these hated foes."},
+            {"Weapon Familiarity": "Gnomes receive a +1 bonus on attack rolls against humanoid creatures of the reptilian and goblinoid subtypes because of their special training against these hated foes."},
+        ]
+        self.skill_bonuses = [
+            {"Keen Senses": "Gnomes receive a +2 racial bonus on Perception checks."},
+            {"Obsessive": " Gnomes receive a +2 racial bonus on a Craft or Profession skill of their choice."},
+        ]
+        self.race_points = 10
+
+class HalfElf(CharacterRace): 
+    def __init__(self):
+        super().__init__()
+        self.name = CoreRace.HALF_ELF.value
+        self.ability_score_modifiers = {
+            "Strength" : 0,
+            "Dexterity" : 0,
+            "Constitution" : 0,
+            "Intelligence" : 0,
+            "Wisdom" : 0,
+            "Charisma" : 0
+        }
+        self.subtype = ["human", "elf"]
+        self.languages.append("Elven")
+        self.senses = {"Low-light vision": "Half-elves can see twice as far as humans in conditions of dim light."}
+        self.racial_abilities = [
+            {"Elven Immunitites": "Half-elves are immune to magic sleep effects and gain a +2 racial saving throw bonus against enchantment spells and effects."},
+            {"Adaptability": "Half-elves receive Skill Focus as a bonus feat at 1st level."},
+            {"Elf Blood": "Half-elves count as both elves and humans for any effect related to race."},
+            {"Multitalented": "Half-elves choose two favored classes at first level and gain +1 hit point or +1 skill point whenever they take a level in either one of those classes"}
+        ]
+        self.skill_bonuses = [
+            {"Keen Senses": "Half-elves receive a +2 racial bonus on Perception checks."}
+        ]
+        self.race_points = 10
+    
+class HalfOrc(CharacterRace): 
+    def __init__(self):
+        super().__init__()
+        self.name = CoreRace.HALF_ORC.value
+        self.ability_score_modifiers = {
+            "Strength" : 0,
+            "Dexterity" : 0,
+            "Constitution" : 0,
+            "Intelligence" : 0,
+            "Wisdom" : 0,
+            "Charisma" : 0
+        }
+        self.subtype = ["human", "orc"]
+        self.languages.append("Orc")
+        self.senses = {"Darkvision": "Half-orcs can see in the dark up to 60 feet."}
+        self.racial_abilities = [
+            {"Orc Ferocity": "Once per day, when a half-orc is brought below 0 hit points but not killed, he can fight on for 1 more round as if disabled. At the end of his next turn, unless brought to above 0 hit points, he immediately falls unconscious and begins dying."},
+            {"Weapon Familiarity": "Half-orcs are proficient with greataxes and falchions and treat any weapon with the word “orc” in its name as a martial weapon."},
+            {"Orc Blood": "Half-orcs count as both humans and orcs for any effect related to race."}
+        ]
+        self.skill_bonuses = [
+            {"Intimidating": "Half-orcs receive a +2 racial bonus on Intimidate checks due to their fearsome nature."}
+        ]
+        self.race_points = 0
+
+class Halfling(CharacterRace): 
+    def __init__(self):
+        super().__init__()
+        self.name = race
+        self.ability_score_modifiers = {
+            "Strength": -2,
+            "Dexterity": 2,
+            "Charisma": 2
+        }
+        self.size = "Small"
+        self.subtype = ["halfling"]
+        self.speed = 20
+        self.languages.append("Halfling")
+        self.racial_abilities = [
+            {"Fearless": "Halflings receive a +2 racial bonus on all saving throws against fear. This bonus stacks with the bonus granted by halfling luck."},
+            {"Halfling Luck": "Halflings receive a +1 racial bonus on all saving throws."},
+            {"Weapon Familiarity": "Halflings are proficient with slings and treat any weapon with the word “halfling” in its name as a martial weapon."}
+        ]
+        self.skill_bonuses = [
+            {"Sure-Footed": "Halflings receive a +2 racial bonus on Acrobatics and Climb checks."},
+            {"Keen Senses": "Halflings receive a +2 racial bonus on Perception checks."}
+        ]
+        self.race_points = 0
+
 class Human(CharacterRace):
     def __init__(self):
         super().__init__()
@@ -162,8 +264,8 @@ class Human(CharacterRace):
             "Intelligence" : 0,
             "Wisdom" : 0,
             "Charisma" : 0
-        } # Add ability to choose +2 to one ability score
-        self.subtype = "(Human)"
+        }
+        self.subtype = ["human"]
         self.racial_abilities = [
             {"Bonus Feat": "Humans select one extra feat at 1st level."}, 
             {"Skilled": "Humans gain an additional skill rank at first level and one additional rank whenever they gain a level."}
