31763c82fd7a08138862bbce07940cdffd565ae8
Laucian1 <resmithjr88@gmail.com>
2025-03-21 22:47:11 -0600

    Tweaked CharacterRace class, added Human, Elf, and Dwarven subclasses

--- race.py
+++ race.py
@@ -76,19 +76,91 @@ class UnknownRace(Enum):
     STRIX = "Strix"
     WAYANG = "Wayang"
 
-class PlayerRace:
+class CharacterRace:
     def __init__(self, race):
-        self.race = race
-        self.ability_bonus = 0
-        self.ability_penalty = 0
+        self.name = race
+        self.ability_score_modifiers = {}
         self.size = "Medium"
         self.type = "Humanoid"
         self.subtype = ""
         self.speed = 30
-        self.s_lang = "Common"
-        self.senses = ""
-        self.def_traits = ""
-        self.off_traits = ""
-        self.skill_bonuses = ""
-        self.sp_su_abilities = ""
-        self.race_points = 11
+        self.languages = ["Common"]
+        self.senses = {}
+        self.racial_abilities = []
+        self.skill_bonuses = []
+        self.race_points = 0
+
+    def apply_racial_traits(self, character):
+        for ability, modifier in self.ability_score_modifiers.items():
+            character.ability_scores[ability] += modifier
+
+        character.size = self.size
+        character.speed = self.speed
+
+        character.languages.extend(self.languages)
+        character.racial_abilities.extend(self.racial_abilities)
+
+class Dwarf(CharacterRace):
+    def __init__(self):
+        super().__init__()
+        self.name = CoreRace.DWARF.value
+        self.ability_score_modifiers = {
+            "Constituion" : 2,
+            "Wisdom" : 2,
+            "Charisma" : -2
+        }
+        self.subtype = "(dwarf)"
+        self.speed = 20
+        self.languages.append("Dwarven")
+        self.senses = {"Darkvision": "Dwarves can see perfectly in the dark up to 60 feet."}
+        self.racial_abilities = [
+            {"Defensive Training": "Dwarves gain a +4 dodge bonus to AC against monsters of the giant subtype."}, 
+            {"Hardy": "Dwarves gain a +2 racial bonus on saving throws against poison, spells, and spell-like abilities."}, 
+            {"Stability": "Dwarves gain a +4 racial bonus to their Combat Maneuver Defense when resisting a bull rush or trip attempt while standing on the ground."}, 
+            {"Hatred": "Dwarves gain a +1 racial bonus on attack rolls against humanoid creatures of the orc and goblinoid subtypes because of their special training against these hated foes."},
+            {"Weapon Familiarity": "Dwarves are proficient with battleaxes, heavy picks, and warhammers, and treat any weapon with the word “dwarven” in its name as a martial weapon."},
+        ]
+        self.skill_bonuses = [
+            {"Greed": "Dwarves gain a +2 racial bonus on Appraise checks made to determine the price of non-magical goods that contain precious metals or gemstones"},
+            {"Stonecunning": "Dwarves gain a +2 bonus on Perception checks to notice unusual stonework, such as traps and hidden doors located in stone walls or floors. They receive a check to notice such features whenever they pass within 10 feet of them, whether or not they are actively looking."},
+        ]
+
+
+class Elf(CharacterRace):
+    def __init__(self):
+        super().__init__()
+        self.name = CoreRace.ELF.value
+        self.ability_score_modifiers = {
+            "Dexterity" : 2,
+            "Constitution" : -2,
+            "Intelligence" : 2
+        }
+        self.subtype = "(elf)"
+        self.languages.append("Elven")
+        self.senses = {"Low-Light Vision": "Elves can see twice as far as humans in conditions of dim light."}
+        self.racial_abilities = [
+            {"Elven Immunities": "Elves are immune to magic sleep effects and gain a +2 racial saving throw bonus against enchantment spells and effects."}, 
+            {"Weapon Familiarity": "Elves are proficient with longbows (including composite longbows), longswords, rapiers, and shortbows (including composite shortbows), and treat any weapon with the word “elven” in its name as a martial weapon."},
+            {"Elven Magic": "Elves receive a +2 racial bonus on caster level checks made to overcome spell resistance. In addition, elves receive a +2 racial bonus on Spellcraft skill checks made to identify the properties of magic items."}
+        ]
+        self.skill_bonuses = {"Keen Senses": "Elves receive a +2 racial bonus on Perception checks"}
+        self.race_points = 10
+
+class Human(CharacterRace):
+    def __init__(self):
+        super().__init__()
+        self.name = CoreRace.HUMAN.value
+        self.ability_score_modifiers = {
+            "Strength" : 0,
+            "Dexterity" : 0,
+            "Constitution" : 0,
+            "Intelligence" : 0,
+            "Wisdom" : 0,
+            "Charisma" : 0
+        } # Add ability to choose +2 to one ability score
+        self.subtype = "(Human)"
+        self.racial_abilities = [
+            {"Bonus Feat": "Humans select one extra feat at 1st level."}, 
+            {"Skilled": "Humans gain an additional skill rank at first level and one additional rank whenever they gain a level."}
+        ]
+        self.race_points = 9
